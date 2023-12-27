8fa9516f27359153dc5b1da9eb84b41a0ba00c26
Krystian <krystian@fullstack-runner.pl>
2023-12-28 00:08:10 +0100

    added navigation js  search false added dot on addewrvntlisenner

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-// const allnavItems = document.querySelectorAll('.nav__items')
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allnavItems = document.querySelectorAll('.nav__item');
 
 const handleNav = () => {
-	nav.classList.toggle('nav--active')
+	nav.classList.toggle('nav--active');
 
-	// allnavItems.forEach(item => {
-	// 	item.addEventListener('click', () => {
-	// 		nav.classList.remove('nav--active')
-	// 	})
-	// })
+	allnavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
 }
 
-navBtn, addEventListener('click', handleNav)
+navBtn.addEventListener('click', handleNav);
